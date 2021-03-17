2f3b496e19c57de04d3a0be71d858bef500e8dc8
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-03-17 09:33:36 -0300

    Primeiro commit

---
+++ src/main/java/com/br/brasilgeoapi/BrasilGeoapiApplication.java
@@ -0,0 +1,13 @@
+package com.br.brasilgeoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasilGeoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasilGeoapiApplication.class, args);
+	}
+
+}
