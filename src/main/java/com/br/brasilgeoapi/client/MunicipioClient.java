85ee8677e65a2609e55760583a27f00016023c8e
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-03-17 13:45:50 -0300

    Client de ServicoDadosIbge

--- src/main/java/com/br/brasilgeoapi/client/MunicipioClient.java
+++
@@ -1,8 +0,0 @@
-package com.br.brasilgeoapi.client;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class MunicipioClient {
-    
-}
---
+++ src/main/java/com/br/brasilgeoapi/client/ServicoDadosIbgeClient.java
@@ -0,0 +1,61 @@
+package com.br.brasilgeoapi.client;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.br.brasilgeoapi.domain.EstadoDto;
+import com.br.brasilgeoapi.domain.MunicipioDto;
+import com.br.brasilgeoapi.exception.BadRequestException;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ServicoDadosIbgeClient {
+
+    private String localidadeApiUrl = "https://servicodados.ibge.gov.br/api/v1/localidades";
+    private RestTemplate restTemplate;
+
+    public ServicoDadosIbgeClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    
+    public List<EstadoDto> buscarTodosEstados() {
+        String url = this.localidadeApiUrl + "/estados";
+
+        ResponseEntity<EstadoDto[]> response = this.restTemplate
+            .exchange(url, HttpMethod.GET, null, EstadoDto[].class);
+        
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new BadRequestException("Erro na consulta de ServiçoDadosIBGE");
+        }
+        return Arrays.asList(response.getBody());
+    }
+
+    public List<MunicipioDto> buscarMunicipiosPorUF(String uf) {
+        String url = this.localidadeApiUrl + "/estados/" + uf + "/municipios";
+
+        ResponseEntity<MunicipioDto[]> response = this.restTemplate
+            .exchange(url, HttpMethod.GET, null, MunicipioDto[].class);
+        
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new BadRequestException("Erro na consulta de ServiçoDadosIBGE");
+        }
+        return Arrays.asList(response.getBody());
+    }
+
+    public List<MunicipioDto> buscarTodosMunicipios() {
+        String url = this.localidadeApiUrl + "/municipios";
+
+        ResponseEntity<MunicipioDto[]> response = this.restTemplate
+            .exchange(url, HttpMethod.GET, null, MunicipioDto[].class);
+        
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new BadRequestException("Erro na consulta de ServiçoDadosIBGE");
+        }
+        return Arrays.asList(response.getBody());
+    }
+}
